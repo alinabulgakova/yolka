61bd75a8c5e3ff0b43f6fe24e8fe0f441229d2d0
alinabulgakova <alinabulgakova@gmail.com>
2020-01-31 23:26:14 +0200

    the last commit

--- src/main/java/Yolka.java
+++ src/main/java/Yolka.java
@@ -1,13 +1,30 @@
 public class Yolka {
     public String print() {
+        int starNum = 1;
         String result = "";
-        for (int i = 0; i <= 1; i++) {
-            if (i == 0) result = "  *  "+"\n";
-            else result += " *** ";
-
+        for (int i = 0; i <= 2; i++) {
+            result += blanks(blanksNum(starNum)) + stars(starNum) + blanks(blanksNum(starNum)) + "\n";
+            starNum += 2;
         }
         return result;
     }
 
+    private int blanksNum(int starNum) {
+        return (7 - starNum) / 2;
+    }
 
+    private String stars(int starNum) {
+        return printSymbols(starNum, "*");
+    }
+
+    private String blanks(int blNum) {
+        return printSymbols(blNum, " ");
+    }
+
+    private String printSymbols(int blanksNumber, String symbol) {
+        String result = "";
+        for (int j = 0; j < blanksNumber; j++)
+            result += symbol;
+        return result;
+    }
 }
--- src/test/java/YolkaTest.java
+++ src/test/java/YolkaTest.java
@@ -6,10 +6,23 @@ public class YolkaTest {
 
     @Test
     public void singleStarPrint() {
-        assertEquals("  *  ", new Yolka().print().split("\n")[0]);
+        assertEquals("   *   ", atLine(0));
     }
 
     @Test
-    public void tripleStarPrint() {assertEquals(" *** ", new Yolka().print().split("\n")[1]);}
+    public void tripleStarPrint() {
+        assertEquals("  ***  ", atLine(1));
+    }
+
+    @Test
+    public void fiveStarsPrint () {
+        assertEquals(" ***** ", atLine(2));
+    }
+
+    private java.lang.String atLine(int line) {
+        return new Yolka().print().split("\n")[line];
+    }
+
+
 
 }
